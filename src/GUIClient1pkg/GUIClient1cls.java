e08aaec428c403d832d15bf68c8b408f1078b2b8
raveen <raveen.rrn@gmail.com>
2015-11-15 22:49:21 -0800

    added more code
    Guiclient1cls

--- src/GUIClient1pkg/GUIClient1cls.java
+++ src/GUIClient1pkg/GUIClient1cls.java
@@ -5,6 +5,7 @@ public class GUIClient1cls {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello GUI client welcome");
+		System.out.println("2nd hello  statement");
 
 	}
 
